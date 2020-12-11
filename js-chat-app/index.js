99351069339bd8a34b7d5f850c4d364a88dd409f
Superandi1234 <43669899+Superandi1234@users.noreply.github.com>
2020-12-11 15:52:19 +0100

    api logic added

--- js-chat-app/index.js
+++ js-chat-app/index.js
@@ -1,31 +1,103 @@
 const STORAGE_KEY = "messages";
-const HERE_API_KEY = ""; // Create a free here account https://developer.here.com/sign-up
+const HERE_API_KEY = "GnyCY3LNNpJLCVB9mxzThsA1nGnNtHzmlhY4CAR1-mg"; // Create a free here account https://developer.here.com/sign-up
 
 window.onload = function () {
-    //TODO 1
+
+    const btn = document.getElementById("sendBtn");
+    btn.onclick = function(event) {
+        event.preventDefault();
+
+        const message = document.getElementById("message");
+        saveMessage(message.value);
+
+        message.value = "";
+        updateTimeline();
+    }
 
     updateTimeline();
 }
 
 function updateTimeline() {
-    //TODO 4
+
+    const messages = getMessagesFromStorage();
+
+    let html = "";
+    messages.forEach(m => {
+        let createdAt = "no time";
+        if(m.createdAt) {
+            createdAt = new Date(m.createdAt).toLocaleDateString();
+        }
+
+        let pos = "unknown position";
+        if(m.adress){
+            pos = m.adress;
+        } else if(m.latitude != null && m.longitude != null) {
+            pos = `(${m.latitude.toFixed(2)}/${m.longitude.toFixed(2)})`;
+        }
+
+        html += `
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    ${m.message}
+                </div>
+                <div class="panel-footer">
+                    <div class="pull-right">
+                        send at ${createdAt} from ${pos}
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>`;
+    });
+
+    const timeline = document.getElementById("timeline");
+    timeline.innerHTML = html;
 }
 
 function saveMessage(message) {
 
-    saveMessageToStorage(message, null, null);
-
-    // TODO 5
+    navigator.geolocation.getCurrentPosition(success => {
+        saveMessageToStorage(message, success.coords.longitude, success.coords.latitude);
+    }, error => {
+        console.error(error);
+        saveMessageToStorage(message, null, null);
+    });
 }
 
 function saveMessageToStorage(message, longitude, latitude) {
-    // TODO 2
 
-    // TODO 6 Use HERE API
+    let address = null;
+    
+    try {
+        address = queryAddressFromHereApi(longitude, latitude);
+
+    } catch(e) {
+        console.warn(e);
+    }
+
+    const messages = getMessagesFromStorage();
+
+    messages.push({
+        message:message,
+        createdAt: new Date().getTime(),
+        latitude: latitude,
+        longitude: longitude,
+        address: null
+    });
+
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(messages));
+
+    updateTimeline();
+
 }
 
 function getMessagesFromStorage() {
-    // TODO 3
+
+    const messages = window.localStorage.getItem(STORAGE_KEY);
+
+    if(!messages) {
+        return [];
+    }
+    return JSON.parse(messages);
 }
 
 function queryAddressFromHereApi(longitude, latitude) {
